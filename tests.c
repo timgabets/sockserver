117a2f14c52b15b0d44f9a20f59845b2900cb89e
Tim Gabets <tim@gabets.ru>
2017-12-08 19:18:18 +0300

    5 calls test case

--- tests.c
+++ tests.c
@@ -3,7 +3,7 @@
 
 #include <unistd.h>
 
-TEST_CASE( "should receive file descriptors received", "" ) {
+TEST_CASE( "should receive file descriptors", "" ) {
   int server_fd = -1;
   int client_fd = -1;
 
@@ -16,4 +16,21 @@ TEST_CASE( "should receive file descriptors received", "" ) {
 
   close(server_fd);
   close(client_fd);
+}
+
+TEST_CASE( "5 consecutive calls should receive should be succsessfull", "" ) {
+  for(int i =0; i < 5; i++){
+    int server_fd = -1;
+    int client_fd = -1;
+
+    getFDs(&server_fd, &client_fd);
+  
+    REQUIRE( server_fd != -1 );
+    REQUIRE( client_fd != -1 );
+    REQUIRE( server_fd > 0 );
+    REQUIRE( client_fd > 0 );
+
+    close(server_fd);
+    close(client_fd);
+  }
 }
