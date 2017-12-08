167be6c2d1dedc3dae43b6064770aaee37e66c40
Tim Gabets <tim@gabets.ru>
2017-12-08 19:39:11 +0300

    'should be able to exchange data with the given file descriptors' test case

--- tests.c
+++ tests.c
@@ -2,6 +2,9 @@
 #include "include/sockserver.h"
 
 #include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 TEST_CASE( "should receive file descriptors", "" ) {
   int server_fd = -1;
@@ -33,4 +36,27 @@ TEST_CASE( "5 consecutive calls should receive should be succsessfull", "" ) {
     close(server_fd);
     close(client_fd);
   }
+}
+
+TEST_CASE( "should be able to exchange data with the given file descriptors", "" ) {
+  int server_fd = -1;
+  int client_fd = -1;
+  
+  getFDs(&server_fd, &client_fd);
+  
+  REQUIRE( server_fd != -1 );
+  REQUIRE( client_fd != -1 );
+  REQUIRE( server_fd > 0 );
+  REQUIRE( client_fd > 0 );
+
+  const char* send_buffer = "iddqd";
+  send(client_fd, send_buffer, strlen(send_buffer), 0);
+
+  char* recv_buffer[128] = {0};
+  recv(server_fd, recv_buffer, 128, 0);
+
+  REQUIRE( strcmp((const char*)recv_buffer, "iddqd") == 0 );
+
+  close(server_fd);
+  close(client_fd);
 }
